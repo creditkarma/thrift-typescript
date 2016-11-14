b62160975092c2edb579e4a5df523a71745291d2
nnance <nance.nick@gmail.com>
2016-11-13 17:45:09 -0800

    working cli

---
+++ src/bin.ts
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+/*
+This is the CLI tool to validate scripts against a schema.  It uses the API found
+in the src/ directory to validate all the files in the file glob CLI parameter.
+ */
+
+const packagejson = require('../package.json')
+import * as program from 'commander'
+import * as fs from 'fs'
+import * as path from 'path'
+import {generateServiceScript} from './index'
+
+program
+  .version(packagejson.version)
+  .alias('thrift-typescript')
+  .usage(`[options] <file.thrift>`)
+  .option('-o, --output <path>', 'Output directory')
+  .description('Generate TypeScript client for Thrift IDL')
+  .parse(process.argv)
+
+async function generateService(fileName) {
+  console.log(`\nParsing Thrift IDL from ${fileName}`)
+  try {
+    const script = await generateServiceScript(fileName)
+    const outputName = path.basename(fileName).replace('thrift', 'ts')
+    fs.writeFileSync(`${program['output']}/${outputName}`, script)
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+}
+
+generateService(program.args[0])
