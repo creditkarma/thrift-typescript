fd6474acd8bea99789d9887657b90c2f9c2027ce
Blaine Bublitz <blaine.bublitz@gmail.com>
2017-04-11 12:41:08 -0500

    temporarily ignore containers (ref #1)

--- src/ts-helpers.ts
+++ src/ts-helpers.ts
@@ -24,6 +24,14 @@ function getStructOrType(type: string) {
   }
 }
 
+function isString(val: any, opts: any) {
+  if (typeof val === 'string') {
+    return opts.fn(this);
+  } else {
+    return opts.inverse(this);
+  }
+}
+
 export function registerHelpers() {
   registerHelper('tsType', (type: string) => tsTypeMap[type] || `ttypes.${type}`)
   registerHelper('upcase', (str: string) => str[0].toUpperCase() + str.substr(1))
@@ -31,4 +39,5 @@ export function registerHelpers() {
   registerHelper('getStructOrType', getStructOrType)
   registerHelper('isStruct', (type: string) => tsTypeMap[type] ? false : true)
   registerHelper('inc', (val: number) => val + 1)
+  registerHelper('isString', isString)
 }
