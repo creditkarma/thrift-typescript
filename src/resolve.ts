3ff4470c3d725a22a849086575935895d8b82533
Blaine Bublitz <blaine.bublitz@gmail.com>
2017-05-27 16:19:04 -0700

    Map namespaces and IDLs to custom types

--- src/resolve.ts
+++
@@ -1,9 +0,0 @@
-
-export function resolveNamespace(idl) {
-  // TODO: the parser doesn't parse dot-separated namespaces
-  const scope = 'js';
-
-  if (idl.namespace && idl.namespace[scope]) {
-    return idl.namespace[scope].serviceName;
-  }
-}
