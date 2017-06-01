23e1b360df05184f50ab8c1cabd847aff0705712
Blaine Bublitz <blaine.bublitz@gmail.com>
2017-06-01 15:05:29 -0700

    Implement initial Union support

---
+++ src/resolve/unions.ts
@@ -0,0 +1,46 @@
+
+import { resolveTypeNode } from './typedefs';
+import { StructNode, StructPropertyNode } from './structs';
+
+import { getUnions } from '../get';
+
+export class UnionPropertyNode extends StructPropertyNode {
+
+  constructor(args) {
+    super(args);
+    // Forced to "optional"
+    // TODO: should we warn here?
+    this.option = 'optional';
+  }
+}
+
+export class UnionNode extends StructNode {
+  public fields: UnionPropertyNode[];
+
+  // TODO: validate single default
+}
+
+export function resolveUnions(idl) {
+  const unions = getUnions(idl);
+
+  return unions.map((union) => {
+    const { name } = union;
+
+    const fields = union.fields.map((field: { id?: number, name: string, type: string, option?: string, defaultValue?: any }) => {
+      return new UnionPropertyNode({
+        id: field.id,
+        name: field.name,
+        type: resolveTypeNode(idl, field.type),
+        option: field.option,
+        defaultValue: field.defaultValue
+      });
+    });
+
+    return new UnionNode({
+      name: name,
+      // TODO: this should be a lookup somehow
+      implements: `${name}Interface`,
+      fields: fields
+    });
+  });
+}
