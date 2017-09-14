1a28159dd43072a18a97137948826f86d2e16a2e
Kevin Greene <kevin.greene@creditkarma.com>
2017-09-14 11:08:49 -0700

    Breakup generator into its own directory

---
+++ src/app/generator/utils/createImportsForIncludes.ts
@@ -0,0 +1,49 @@
+import * as ts from 'typescript'
+import * as path from 'path'
+
+import {
+  IIncludeMap,
+  IResolvedIncludeMap,
+  IResolvedIdentifier,
+  IRenderedFile
+} from '../../types'
+
+export function createImportsForIncludes(
+  currentPath: string,
+  includes: IIncludeMap,
+  resolved: IResolvedIncludeMap,
+): Array<ts.ImportDeclaration> {
+  const imports: Array<ts.ImportDeclaration> = []
+  for (const name of Object.keys(resolved)) {
+    const resolvedIncludes: Array<IResolvedIdentifier> = resolved[name]
+    const includeFile: IRenderedFile = includes[name]
+
+    if (resolvedIncludes != null && includeFile != null) {
+      imports.push(ts.createImportDeclaration(
+        undefined,
+        undefined,
+        ts.createImportClause(
+          undefined,
+          ts.createNamedImports(
+            resolvedIncludes.map((next: IResolvedIdentifier) => {
+              return ts.createImportSpecifier(
+                ts.createIdentifier(next.name),
+                ts.createIdentifier(next.resolvedName),
+              )
+            }),
+          ),
+        ),
+        ts.createLiteral(
+          `./${path.join(
+            path.relative(
+              path.dirname(currentPath),
+              path.dirname(includeFile.outPath),
+            ),
+            path.basename(includeFile.outPath, '.ts'),
+          )}`,
+        ),
+      ))
+    }
+  }
+  return imports
+}
