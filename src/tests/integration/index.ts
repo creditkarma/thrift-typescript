7c1ee7a352d3a56303c850b42058b2fcc1918df7
Kevin Greene <kevin.greene@creditkarma.com>
2017-10-03 15:01:01 -0700

    Add integration tests to circleci

--- src/tests/integration/index.ts
+++ src/tests/integration/index.ts
@@ -16,7 +16,7 @@ const serverProc = fork('./server.ts')
 function exit(code: number) {
   clientProc.kill()
   serverProc.kill()
-  process.exitCode = code
+  process.exit(code)
 }
 
 setTimeout(() => {
@@ -28,8 +28,13 @@ setTimeout(() => {
 
     if (stout === 'status: 1') {
       exec('curl http://localhost:8044/peg', function(err, stout, sterr) {
-        console.log('Successfully able to run Thrift service')
-        exit(0)
+        if (stout === 'peg complete') {
+          console.log('Successfully able to run Thrift service')
+          exit(0)
+        } else {
+          console.error(`Unexpected response from Thrift service: ${stout}`)
+          exit(1)
+        }
       })
     } else {
       console.error(`Unexpected response from Thrift service: ${stout}`)
@@ -37,3 +42,8 @@ setTimeout(() => {
     }
   })
 }, 5000)
+
+setTimeout(() => {
+  console.error(`Timeout while running integration tests`)
+  exit(1)
+}, 10000)
