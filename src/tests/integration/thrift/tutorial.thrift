9abe7d5f5088f508c5549db4f39abb2e8db29407
Kevin Greene <kevin.greene@creditkarma.com>
2017-11-02 11:37:32 -0700

    Fixing tests

--- src/tests/integration/thrift/tutorial.thrift
+++ src/tests/integration/thrift/tutorial.thrift
@@ -99,7 +99,7 @@ const map<string,string> MAPCONSTANT = {'hello':'world', 'goodnight':'moon'}
 struct Work {
   1: i32 num1 = 0,
   2: i32 num2,
-  3: operation.Operation op,
+  3: Operation op,
   4: optional string comment,
 }
 
