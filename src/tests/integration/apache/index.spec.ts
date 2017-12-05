14a393d9e5ef84899899c9b7a5cff841c623fa9f
Kevin Greene <kevin.greene@creditkarma.com>
2017-12-05 09:26:41 -0800

    Cleanup integration tests

--- src/tests/integration/apache/index.spec.ts
+++ src/tests/integration/apache/index.spec.ts
@@ -1,10 +1,41 @@
 import { assert } from 'chai'
-import { exec } from 'child_process'
 
-import './client'
+import {
+  HttpConnection,
+  createHttpClient,
+  createHttpConnection,
+  TBufferedTransport,
+  TBinaryProtocol,
+  Int64,
+} from 'thrift'
+
+import {
+  Calculator,
+  Operation,
+  Work,
+} from './codegen/calculator/calculator'
+
 import './server'
 
+import {
+  SERVER_CONFIG
+} from './config'
+
 describe('Thrift TypeScript', () => {
+  const options = {
+    transport: TBufferedTransport,
+    protocol: TBinaryProtocol,
+    https: false,
+    headers: {
+      Host: SERVER_CONFIG.hostName,
+    }
+  }
+  const connection: HttpConnection = createHttpConnection(SERVER_CONFIG.hostName, SERVER_CONFIG.port, options)
+  const thriftClient: Calculator.Client = createHttpClient(Calculator.Client, connection)
+
+  connection.on('error', (err: Error) => {
+    process.exit(1)
+  })
 
   // Allow servers to spin up
   before((done) => {
@@ -12,23 +43,48 @@ describe('Thrift TypeScript', () => {
   })
 
   it('should call an endpoint with no arguments', (done) => {
-    exec('curl "http://localhost:8044/ping"', (err, stout, sterr) => {
-      assert.equal(stout, 'success')
+    thriftClient.ping().then((val: any) => {
+      assert.equal(val, undefined)
       done()
-    });
+    }, (err: any) => {
+      done(err)
+    })
   })
 
   it('should correctly call endpoint with arguments', (done) => {
-    exec('curl "http://localhost:8044/calculate?left=3&op=add&right=5"', (err, stout, sterr) => {
-      assert.equal(stout, 'result: 8')
+    const add: Work = new Work({
+      num1: 4,
+      num2: 8,
+      op: Operation.ADD
+    })
+
+    const subtract: Work = new Work({
+      num1: 67,
+      num2: 13,
+      op: Operation.SUBTRACT
+    })
+
+    Promise.all([
+      thriftClient.calculate(1, add),
+      thriftClient.calculate(1, subtract)
+    ]).then((val: Array<number>) => {
+      assert.equal(val[0], 12)
+      assert.equal(val[1], 54)
       done()
+    }, (err: any) => {
+      done(err)
     })
   })
 
   it('should correctly call endpoint with i64 args', (done) => {
-    exec('curl "http://localhost:8044/add?left=5&right=6"', (err, stout, sterr) => {
-      assert.equal(stout, 'result: 11')
+    const left: Int64 = new Int64(5)
+    const right: Int64 = new Int64(3)
+
+    thriftClient.add(left, right).then((val: Int64) => {
+      assert.equal(val.toNumber(), 8)
       done()
+    }, (err: any) => {
+      done(err)
     })
   })
 })
