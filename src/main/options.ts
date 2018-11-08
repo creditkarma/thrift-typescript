43cb0f9bc9cc7a29ec331789c8ccd4331f52980f
Kevin Greene <kevin.greene@creditkarma.com>
2018-11-08 08:47:01 -0800

    chore: make default_options immutable

--- src/main/options.ts
+++ src/main/options.ts
@@ -1,10 +1,10 @@
 import { IMakeOptions } from './types'
 
-export const DEFAULT_OPTIONS: IMakeOptions = {
+export const DEFAULT_OPTIONS: IMakeOptions = Object.freeze({
     rootDir: '.',
     outDir: './codegen',
     sourceDir: './thrift',
     target: 'apache',
     files: [],
     fallbackNamespace: 'java',
-}
+} as IMakeOptions)
